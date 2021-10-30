639b3aaca269ad552972c0ac54263da3bbf456e4
Vanaldito <fredimanuelb@gmail.com>
2021-10-30 15:59:45 -0600

    second commit

---
+++ game/board.py
@@ -0,0 +1,8 @@
+class Board:
+    """ A class to manage the board """
+
+    def __init__(self):
+        """ Create a new board """
+
+    def update(self):
+        """ Draw the board on the screen """
